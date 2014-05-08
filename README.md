ecab1770a2278ba1aa0697a102eca81975ed045c
Bill Seitz <bill@fluxent.com>
2014-05-08 15:24:10 -0500

    Update schema.sql and add startup instructions in README.

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Using SQLite-3.
 
 See BillSeitz notes at http://webseitz.fluxent.com/wiki/ExtendingWebpyBlogAppWithCookbookFeatures
 
+Before launching the code for the first time:
+* copy "config_example.py" to "config.py" - and tweak SMTP values if you want to use that feature
+* generate empty SQLite db from schema: "sqlite3 blog.db < schema.sql"
--- schema.sql
+++ schema.sql
@@ -1,6 +1,15 @@
 CREATE TABLE "entries" (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NULL,
     title TEXT,
     content TEXT,
     posted_on DATETIME
 );
+CREATE TABLE "users" (
+    user_id             INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    user_name          varchar(64) NOT NULL,
+    user_password       varchar(255) NOT NULL,
+    user_email          varchar(64),
+    user_status         varchar(16) NOT NULL DEFAULT 'active',
+    user_last_login     datetime NOT NULL, 
+    hash_temp           varchar(64) NULL, create_date datetime, update_date datetime, cluster integer);
